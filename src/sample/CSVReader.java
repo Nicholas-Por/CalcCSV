63ae7e2f286485b5dd6fdc6a6589e8d915e0ce07
BT_1N3_02@bktech.local <BT_1N3_02@bktech.local>
2019-02-06 10:39:56 -0500

    Initial commit

---
+++ src/sample/CSVReader.java
@@ -0,0 +1,33 @@
+package sample;
+//this will have to change according to how you update the text
+//output file, or rather the CSV file
+public class CSVReader {
+}
+    // Java code to illustrate reading a
+// CSV file line by line
+ public static void readDataLineByLine(String file)
+    {
+
+        try {
+
+            // Create an object of filereader
+            // class with CSV file as a parameter.
+            FileReader filereader = new FileReader(file);
+
+            // create csvReader object passing
+            // file reader as a parameter
+            CSVReader csvReader = new CSVReader(filereader);
+            String[] nextRecord;
+
+            // we are going to read data line by line
+            while ((nextRecord = csvReader.readNext()) != null) {
+                for (String cell : nextRecord) {
+                    System.out.print(cell + "\t");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
